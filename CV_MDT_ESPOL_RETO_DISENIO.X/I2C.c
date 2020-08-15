f030bd6da705db3acd6695687bc09312d360c81e
ChrisVaccaro <cvaccaro@espol.edu.ec>
2020-08-15 12:29:20 -0500

    i2cFiles
    Archivos para funciones I2C agregadas

---
+++ CV_MDT_ESPOL_RETO_DISENIO.X/I2C.c
@@ -0,0 +1,106 @@
+/*
+ * File:   I2C.c
+ * Author: MDTECHNOLOGY <www.mdtechnology.com.ec>
+ *
+ * Created on August 8, 2020, 1:02 AM
+ */
+
+#include "I2C.h"
+
+void I2C_Initialize(void){
+    TRISAbits.TRISA2 = 1;
+    TRISAbits.TRISA4 = 1;  
+    
+    SSPDATPPS = 0X04;   //RA4->MSSP:SDA;    
+    RA4PPS = 0X11;   //RA4->MSSP:SDA;    
+    RA2PPS = 0X10;   //RA2->MSSP:SCL;    
+    SSPCLKPPS = 0X02 ;   //RA2->MSSP:SCL;    
+    
+    return;
+}
+
+void I2C_Master_Initialize(void){
+    SSP1STAT = 0x80;
+    SSP1CON1 = 0x08;
+    SSP1CON2 = 0x00;
+    SSP1CON3 = 0x00; 
+    SSP1ADD = (uint8_t)((_XTAL_FREQ/(4*I2C_bps))-1);
+    
+    SSP1CON1bits.SSPEN = 1; 
+    
+    return; 
+}
+
+void I2C_Slave_Initialize(short address){
+    SSP1STAT        = 0x80;
+    SSP1CON1        = 0x26;
+    SSP1CON2        = 0x00;
+    SSP1CON3        = 0x00; 
+    SSP1ADD         = address;
+    INTCONbits.GIE  = 1;
+    INTCONbits.PEIE = 1;
+    PIR1bits.SSP1IF = 0;
+    PIE1bits.SSP1IE = 1;
+
+    return;
+}
+
+void I2C_Start(void){
+    SSP1CON2bits.SEN= 1;            
+    while (SSP1CON2bits.SEN);
+    
+    return;
+}
+
+void I2C_Restart(void){
+    SSP1CON2bits.RSEN = 1;          
+    while (SSP1CON2bits.RSEN); 
+    
+    return;
+}
+
+void I2C_Stop(void){
+    SSP1CON2bits.PEN = 1; 
+    while (SSP1CON2bits.PEN); 
+    
+    return;
+}
+
+void I2C_ACK(void){
+    SSP1CON2bits.ACKDT = 0; 
+    SSP1CON2bits.ACKEN = 1; 
+    while (SSP1CON2bits.ACKEN); 
+    
+    return;
+}
+
+void I2C_NACK(void){
+    SSP1CON2bits.ACKDT = 1; 
+    SSP1CON2bits.ACKEN = 1; 
+    while (SSP1CON2bits.ACKEN); 
+    
+    return;
+}
+
+void I2C_Write(uint8_t data){
+    PIR1bits.SSP1IF = 0; 
+    SSP1BUF = data;
+    while (!PIR1bits.SSP1IF); 
+
+    return;
+}
+
+uint8_t I2C_Read(void){
+    uint8_t data_rx     = 0x00;
+
+    data_rx             = SSP1BUF;
+    SSP1CON2bits.RCEN   = 1; 
+    PIR1bits.SSP1IF     = 0;
+    while(!SSP1STATbits.BF);
+    
+    data_rx             = SSP1BUF;
+    SSP1CON2bits.ACKDT  = 1;
+    SSP1CON2bits.ACKEN  = 1;
+    
+    return data_rx;
+}
